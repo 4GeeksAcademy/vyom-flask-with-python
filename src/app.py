a8a95af862f564b95f8333d7c4c1be2231f44dae
Amat98 <108639529+Amat98@users.noreply.github.com>
2024-09-02 01:24:10 +0000

    Creando modelos y endpoints para agregar y/o eliminar a una lista de favoritos

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet, Starship, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -36,6 +36,8 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
@@ -44,6 +46,327 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# PIDE LOS DATOS DE TODOS LOS USUARIOS CREADOS
+@app.route('/users')
+def get_users():
+    try:
+        users = User.query.all()
+
+        if not users:
+            return jsonify({'error': 'No se encuentra resultados'}), 404
+        
+        list_users = []
+
+        for user in users:
+            list_users.append(user.serialize())
+        return jsonify(list_users), 200
+    
+    except Exception as e:
+        return jsonify({'error': 'falla en el servidor'}), 500
+
+
+# PIDE LOS DATOS DE TODOS LOS PERSONAJES CREADOS
+@app.route('/people')
+def get_people():
+    try:
+        characters = Character.query.all()
+
+        if not characters:
+            return jsonify({
+                'Error': "No se encuentran personajes"
+            }), 400
+        
+        list_people = []
+        for character in characters:
+            list_people.append(character.serialize())
+        
+        return jsonify(list_people), 200
+    except Exception as e:
+        return jsonify({'error', 'Falla en el servidor'}), 500
+
+# PIDE LOS DATOS DE UN PERSONAJE CON SU ID
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_people_id(people_id):
+    try:
+        character = Character.query.get(people_id)
+        
+        if character:
+            return jsonify(character.serialize()), 200
+        else:
+            return jsonify({'Error': 'Personaje no econtrado'}), 404
+        
+    except Exception as e:
+        return jsonify({'Error', 'Falla en el servidor'}), 500
+
+# PIDE LOS DATOS DE TODOS LOS PLANETAS CREADOS
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    try:
+        planets = Planet.query.all()
+        if not planets:
+            return jsonify({'error': 'No se encontro planetas'}), 404
+        
+        list_planets = []
+        for planet in planets:
+            list_planets.append(planet.serialize())
+        
+        return jsonify(list_planets), 200
+    
+    except Exception as e:
+        return jsonify({'error', 'Falla en el servidor'}), 500
+
+# PIDE LOS DATOS DE UN PLANETA CON SU ID
+@app.route('/planets/<int:planet_id>', methods=["GET"])
+def get_planet(planet_id):
+    try:
+        planet = Planet.query.get(planet_id)
+
+        if planet:
+            return jsonify(planet.serialize()), 200
+        else:
+            return jsonify({'error': 'No se encuentra este planeta'}), 404
+        
+    except Exception as e:
+        return jsonify({'error': 'Falla en el servidor'}), 500
+
+#PEDIMOS TODAS LAS NAVES DISPONIBLES
+@app.route('/starships', methods=['GET'])
+def get_starships():
+    try:
+        starships = Starship.query.all()
+        if not starships:
+            return jsonify({'error': 'No se encontraron naves'}), 404
+
+        list_starships = [starship.serialize() for starship in starships]
+        return jsonify(list_starships), 200
+
+    except Exception as e:
+        return jsonify({'error': 'Falla en el servidor'}), 500
+
+# PEDIMOS INFO DE UNA NAVE POR ID
+@app.route('/starships/<int:starship_id>')
+def get_starship_id(starship_id):
+    try:
+        startship =  Starship.query.get(starship_id)
+
+        if startship:
+            return jsonify(startship.serialize()), 200
+        else:
+            return jsonify({'error': 'No se encuentra esta nave'}), 404
+        
+    except Exception as e:
+        return jsonify({'error': 'Falla en el servidor'}), 500
+
+# PEDIMOS LA LISTA DE FAVORITOS DE USUARIO CURRENT_USER_ID
+@app.route('/users/favorites', methods=['GET'])
+def get_favorites():
+    current_user_id = 5
+    try:
+
+        
+        favorites =  Favorite.query.filter_by(user_id=current_user_id).all()
+
+        if not favorites:
+            return jsonify({'error': 'Este usuario no tiene favoritos'}), 400
+        
+        list_favorites = [favorite.serialize() for favorite in favorites]
+
+        return jsonify(list_favorites), 200
+        
+    except Exception as e:
+        return jsonify({'error': 'Falla en el servidor'}), 500
+
+# ANADIR PLANETA A LISTA DE FAVORITOS DE USUARIO CURRENT_USER_ID
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(planet_id):
+    current_user_id = 5
+
+    try:
+        planet = Planet.query.get(planet_id)
+        if not planet:
+            return jsonify({'error': 'Planeta no encontrado'}), 404
+        
+        existing_favorite = Favorite.query.filter_by(
+            user_id=current_user_id,
+            planet_id=planet_id
+        ).first()
+        
+        if existing_favorite:
+            return jsonify({'error': 'Este planeta ya está en los favoritos del usuario'}), 400
+        
+        new_favorite = Favorite(
+            user_id=current_user_id,
+            planet_id=planet_id
+        )
+        
+        db.session.add(new_favorite)
+        db.session.commit()
+
+        return jsonify(new_favorite.serialize()), 201
+    
+    except Exception as e:
+        return jsonify({'error': "falla en el servidor"}), 500
+
+# ANADIR PERSONAJE FAVORITO A LISTA DE USUARIO SELECCIONADO EN CURRENT_USER_ID
+@app.route('/favorite/people/<int:people_id>', methods=["POST"])
+def add_people_favorite(people_id):
+    current_user_id = 5
+
+    try:
+        character = Character.query.get(people_id)
+        if not character:
+            return jsonify({'error': 'Personaje no encontrado en la lista'}), 404
+    
+        existing_favorite = Favorite.query.filter_by(
+            user_id = current_user_id,
+            character_id = people_id
+        ).first()
+
+        if existing_favorite:
+            return jsonify({'error': f'Este personaje ya esta en los favoritos del usuario: {current_user_id}'}), 400
+
+        new_favorite = Favorite(
+            user_id =  current_user_id,
+            character_id = people_id
+        )
+
+        db.session.add(new_favorite)
+        db.session.commit()
+
+        return jsonify(new_favorite.serialize()), 201
+        
+    except Exception as e:
+        return jsonify({'error': 'Falla en el servidor'}), 500
+    
+# ANADIR NAVE FAVORITA A LISTA DE USUARIO SELECCIONADO EN CURRENT_USER_ID
+@app.route('/favorite/starship/<int:starship_id>', methods=['POST'])
+def add_startship_favorite(starship_id):
+    current_user_id = 5
+    try:
+        starship = Starship.query.get(starship_id)
+        if not starship:
+            return jsonify({'error': 'Nave no encontrada'}), 404
+
+        existing_favorite = Favorite.query.filter_by(
+            user_id = current_user_id,
+            starship_id = starship_id
+        ).first()
+
+        if existing_favorite:
+            return jsonify({'error': f'Esta nave ya esta en los favoritos del usuario: {current_user_id}'})
+        
+        new_favorite = Favorite(
+            user_id = current_user_id,
+            starship_id = starship_id
+        )
+
+        db.session.add(new_favorite)
+        db.session.commit()
+
+        return jsonify(new_favorite.serialize()), 201
+        
+    except Exception as e:
+        return jsonify({'error': 'Falla en el servidor'}), 500
+
+
+# ELIMINAR PERSONAJE DE LA LISTA DE USUARIO SELECCIONADO EN CURRENT_USER_ID
+@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
+def delete_character_user(people_id):
+    current_user_id = 5
+    try:
+        character = Character.query.get(people_id)
+        if not character:
+            return jsonify({'error': 'Personaje no encontrado'}), 404
+        
+        existing_favorite = Favorite.query.filter_by(
+            user_id = current_user_id,
+            character_id = people_id
+        ).first()
+
+        if existing_favorite:
+            db.session.delete(existing_favorite)
+            db.session.commit()
+            
+            updated_favorites = Favorite.query.filter_by(user_id=current_user_id).all()
+            favorites_list = [favorite.serialize() for favorite in updated_favorites]
+
+            return jsonify({
+                'message': 'Personaje eliminado de favoritos',
+                'nueva_lista': favorites_list
+            }), 202
+
+        return jsonify({'error': f'El persona no esta en los favoritos del usuario con id: {current_user_id}'}), 400
+
+        
+    except Exception as e:
+        return jsonify({'error': 'Falla en el servidor'}), 500
+
+
+# ELIMINAR PLANETA DE LA LISTA DE USUARIO SELECCIONADO EN CURRENT_USER_ID
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_planet_user(planet_id):
+    current_user_id = 5
+    try:
+        planet = Planet.query.get(planet_id)
+        if not planet:
+            return jsonify({'error': 'Planeta no encontrado'}), 404
+
+        existing_favorite = Favorite.query.filter_by(
+            user_id = current_user_id,
+            planet_id = planet_id
+        ).first()
+
+        if existing_favorite:
+            db.session.delete(existing_favorite)
+            db.session.commit()
+
+            updated_favorites = Favorite.query.filter_by(user_id = current_user_id).all()
+            favorites_list = [favorite.serialize() for favorite in updated_favorites]
+
+            return jsonify({
+                'message': 'Planeta eliminado de favoritos',
+                'Nueva_lista': favorites_list
+            }), 202
+
+        return jsonify({'error': f'El planeta no esta en los favoritos del usuario con id: {current_user_id}'}), 400
+        
+    except Exception as e:
+        return jsonify({'error': 'Falla en el servidor'}), 500
+
+
+# ELIMINAR NAVE DE LA LISTA DE USUARIO SELECCIONADO EN CURRENT_USER_ID
+@app.route('/favorite/starship/<int:starship_id>', methods=['DELETE'])
+def delete_starship_user(starship_id):
+    current_user_id = 5
+    try:
+        starship = Starship.query.get(starship_id)
+        if not starship:
+            return jsonify({'error': 'Nave no encontrada'}), 404
+
+        existing_favorite = Favorite.query.filter_by(
+            user_id = current_user_id,
+            starship_id = starship_id
+        ).first()
+
+        if existing_favorite:
+            db.session.delete(existing_favorite)
+            db.session.commit()
+
+            updated_favorites = Favorite.query.filter_by(user_id = current_user_id). all()
+            favorites_list = [favorite.serialize() for favorite in updated_favorites]
+
+            return jsonify({
+                'message': 'Nave eliminada de favoritos',
+                'nueva_lista': favorites_list
+            }), 202
+
+        return jsonify({'error': f'La nave no esta en los favoritos del usuario con id: {current_user_id}'}), 400
+    
+    except Exception as e:
+        return jsonify({'error': 'Falla en el servidor'}), 500
+
+# @app.route('')
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,16 +4,104 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    # starships = db.relationship('Starship', backref='users', lazy=True)
+    # planet = db.relationship('Planet', backref='users', lazy=True)
+    # favorites = db.relationship('Favorite', backref='user', lazy=True)
+
+
+
+
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
+            "user_name": self.user_name,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            # do not serialize the password, it's a security breach
         }
+
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    # planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=False)
+    # planet = db.relationship('Planet', backref='character', lazy=True)
+
+
+    def __repr__(self):
+        return '<Character %r>' % self.id
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    climate = db.Column(db.String(100), nullable=True)
+    
+    def __repr__(self):
+        return '<Planet %r>' % self.id
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate
+        }
+
+class Starship(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    # character_id  = db.Column(db.Integer, db.ForeignKey('character.id'), nullable=False)
+    # character = db.relationship('Character', backref='starship', lazy=True)
+
+    def __repr__(self):
+        return '<Starship %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # "character_id": self.character_id
+        }
+
+class Favorite(db.Model): 
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=True)
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'), nullable=True)
+    starship_id = db.Column(db.Integer, db.ForeignKey('starship.id'), nullable=True)
+
+    user = db.relationship('User', backref='favorites')
+    planet = db.relationship('Planet', backref='favorites')
+    character = db.relationship('Character', backref='favorites')
+    starship = db.relationship('Starship', backref='favorites')
+
+    def __repr__(self):
+        return '<Favorite %r>' % self.id
+
+    def serialize(self):
+        serialized_data = {
+            "id": self.id,
+            "user_id": self.user_id
+        }
+        if self.planet_id is not None:
+            serialized_data["planet_id"] = self.planet_id
+        if self.character_id is not None:
+            serialized_data["character_id"] = self.character_id
+        if self.starship_id is not None:
+            serialized_data["starship_id"] = self.starship_id
+
+        return serialized_data
